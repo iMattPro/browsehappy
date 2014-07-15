dbe1d5d060512cec7aea8338a7f90d7c2fceadc0
Matt Friedman <maf675@gmail.com>
2014-07-14 20:50:26 -0700

    Add Turkish translation

---
+++ language/tr/browsehappy.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* Browse Happy [Turkish]
+*
+* @copyright (c) 2013 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'BROWSEHAPPY_TITLE'		=> 'Modası geçmiş bir tarayıcı kullanıyorsunuz!',
+	'BROWSEHAPPY_MESSAGE'	=> 'Siteyi daha iyi ve daha güvenli bir şekilde kullanabilmek için lütfen modern bir tarayıcıya terfi edin.',
+	'BROWSEHAPPY_UPGRADE'	=> 'Daha modern bir tarayıcıya terfi edin',
+	'BROWSEHAPPY_WARNING'	=> 'Dikkat!',
+));
