1175ecb1ce95449cdac1901b59411155d41e3e82
Meisam Nobari <meis2m@yahoo.com>
2014-09-28 11:30:07 +0330

    persian translation

---
+++ language/fa/browsehappy.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*
+* Browse Happy [Persian]
+* Persian Translator: Meisam nobari in www.php-bb.ir
+* @copyright (c) 2013 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+$lang = array();
+}
+$lang = array_merge($lang, array(
+'BROWSEHAPPY_TITLE'	=> 'شما از یک مرورگر قدیمی و منسوخ شده استفاده میکنید.',
+'BROWSEHAPPY_MESSAGE'	=> 'جهت استفاده بهتر ، مطمئن تر و یک تجربه عالی از انجمن لطفا مرورگر خود را آپدیت کنید.',
+'BROWSEHAPPY_UPGRADE'	=> 'ارتقا به یک مرورگر مدرن و جدید',
+'BROWSEHAPPY_WARNING'	=> 'هشدار !',
+));
