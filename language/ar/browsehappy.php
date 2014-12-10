adfcfef6dcfb5470ca73e026412eb071e6cdde3e
alhitary <alhitary@alhitary.net>
2014-12-11 00:00:32 +0300

    Create browsehappy.php

---
+++ language/ar/browsehappy.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* Browse Happy [Arabic]
+*
+* @copyright (c) 2013 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* Translated By : Basil Taha Alhitary - www.alhitary.net
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'BROWSEHAPPY_TITLE'		=> 'أنت تستخدم متصفح قديم جداً !',
+	'BROWSEHAPPY_MESSAGE'	=> 'لتصفح هذا الموقع بشكل أفضل وآمن , يجب عليك تحديث متصفحك أو تنصيب متصفح متطور و حديث.',
+	'BROWSEHAPPY_UPGRADE'	=> 'يجب عليك تحديث المتصفح لديك',
+	'BROWSEHAPPY_WARNING'	=> 'تحذير !',
+));
