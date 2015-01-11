03a58d80e8fdd1f5a79fbee86a3b42a7d0e5772b
Matt Friedman <maf675@gmail.com>
2015-01-10 21:50:50 -0800

    Add Polish translation

---
+++ language/pl/browsehappy.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* Browse Happy [Polish]
+*
+* @copyright (c) 2013 Matt Friedman, Translated by bielu
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'BROWSEHAPPY_TITLE'		=> 'Używasz przestarzałej przeglądrki!',
+	'BROWSEHAPPY_MESSAGE'	=> 'Dla lepszych i bezpieczniejszych doznań z użytkowania tej strony, prosimy o używanie nowoczesnej przeglądarki.',
+	'BROWSEHAPPY_UPGRADE'	=> 'Zaktualizuj do nowoczesnej przeglądarki',
+	'BROWSEHAPPY_WARNING'	=> 'Uwaga!',
+));
