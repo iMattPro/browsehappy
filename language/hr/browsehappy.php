88cb7775eda702b45e2f086b9a450d6020016920
Matt Friedman <maf675@gmail.com>
2015-01-29 11:52:10 -0800

    Add Croatian language support

---
+++ language/hr/browsehappy.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* Browse Happy [Croatian]
+* Croatian translation by Ančica Sečan (http://ancica.sunceko.net)
+*
+* @copyright (c) 2013 Matt Friedman
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'BROWSEHAPPY_TITLE'		=> 'Koristiš zastarjeli preglednik!',
+	'BROWSEHAPPY_MESSAGE'	=> 'Za bolje i sigurnije korištenje ovih stranica, molim(o), nadogradi zastarjeli preglednik modernim.',
+	'BROWSEHAPPY_UPGRADE'	=> 'Nadogradi preglednik modernim',
+	'BROWSEHAPPY_WARNING'	=> 'Upozorenje!',
+));
